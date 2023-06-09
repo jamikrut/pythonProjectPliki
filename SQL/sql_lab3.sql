0ff4a38baec06f0149c412140f3faf145d387eaf
jamikrut <jamikrut@student.wszib.edu.pl>
2023-06-09 14:01:55 +0200

    Updated sql_lab3

--- SQL/sql_lab3.sql
+++ SQL/sql_lab3.sql
@@ -2,31 +2,61 @@ USE AdventureWorks2014
 GO
 
 -- 1. Jakie są średnie roczne zarobki pracowników [HumanResources.Employee] Adventure Works według stanowisk? [humanresources.employeepayhistory]
-SELECT *
+SELECT HRE.JobTitle, AVG(HRPH.Rate) AS AveragePayPerHour, AVG(HRPH.Rate * 40 * 52) AS AproxAveragePayPerYear
 FROM HumanResources.Employee HRE
-         LEFT JOIN HumanResources.EmployeePayHistory EPH on HRE.BusinessEntityID = EPH.BusinessEntityID
---??
+         LEFT JOIN HumanResources.EmployeePayHistory HRPH ON HRE.BusinessEntityID = HRPH.BusinessEntityID
+GROUP BY HRE.JobTitle
 
 -- 2. Jakie jest łączne zamówienie dla każdego klienta, który kupił produkty Adventure Works w 2013 roku? [Sales.SalesOrderHeader]
+SELECT SC.CustomerID, SUM(SOH.TotalDue) AS CustomerTotalDue
+FROM Sales.Customer SC
+         LEFT JOIN Sales.SalesOrderHeader SOH ON SC.CustomerID = SOH.CustomerID
+WHERE YEAR(SOH.OrderDate) = 2012
+GROUP BY SC.CustomerID
 
 
 -- 3. Ile zamówień zostało złożonych przez każdego pracownika Adventure Works? [Sales.SalesOrderHeader]
-
+SELECT SOH.CustomerID, COUNT(*) AS OrderCount
+FROM Sales.SalesOrderHeader SOH
+         LEFT JOIN Sales.Customer SC ON SOH.CustomerID = SC.CustomerID
+         LEFT JOIN Person.Person PP ON SC.PersonID = PP.BusinessEntityID
+         INNER JOIN HumanResources.Employee HRE ON PP.BusinessEntityID = HRE.BusinessEntityID
+GROUP BY SOH.CustomerID
 
 -- 4. Jaka jest łączna ilość zamówień (LineTotal) dla każdej kategorii produktów Adventure Works? [Sales.SalesOrderDetail] [Production.Product]
+SELECT PSC.ProductCategoryID, SUM(SOD.LineTotal) AS SumLineTotal
+FROM Sales.SalesOrderDetail SOD
+         LEFT JOIN Production.Product PP ON PP.ProductID = SOD.ProductID
+         LEFT JOIN Production.ProductSubcategory PSC
+                   ON PSC.ProductSubcategoryID = PP.ProductSubcategoryID
+GROUP BY PSC.ProductCategoryID
 
 
 -- 5. Znajdź średnią wartość zamówienia dla każdego klienta w 2012 roku [Sales.Customer] [Sales.SalesOrderHeader]
-
+SELECT SC.CustomerID, AVG(SOH.TotalDue) AS AverageTotalDue
+FROM Sales.Customer SC
+         LEFT JOIN Sales.SalesOrderHeader SOH ON SC.CustomerID = SOH.CustomerID
+WHERE YEAR(SOH.OrderDate) = 2012
+GROUP BY SC.CustomerID
 
 -- 6. Znajdź trzy najlepiej sprzedające się produkty (w największej ilości orderqty) w 2016 roku [Production.Product] [Sales.SalesOrderDetail] [Sales.SalesOrderHeader]
-
+SELECT TOP 3 PP.ProductID, PP.Name AS ProductNAme, SUM(SOD.OrderQty) AS TotalSoldAmount
+FROM Production.Product PP
+         LEFT JOIN Sales.SalesOrderDetail SOD ON SOD.ProductID = PP.ProductID
+         LEFT JOIN Sales.SalesOrderHeader SOH ON SOD.SalesOrderID = SOH.SalesOrderID
+WHERE YEAR(SOH.OrderDate) = 2016 --2014
+GROUP BY PP.ProductID, PP.Name
+ORDER BY SUM(SOD.OrderQty) DESC
 
 -- 7. Znajdź średnią wartość zamówienia dla każdego kraju. Weź pod uwagę tylko klientów którzy dokonali co najmniej 10 zamówień (subquery) [Sales.Customer] [Sales.SalesOrderHeader]
 
 
 -- 8. Znajdź 10 najlepszych klientów pod względem wartości zamówień (TotalDue) [Sales.Customer] [Sales.SalesOrderHeader]
-
+SELECT TOP 10 SC.CustomerID, SC.PersonID, SUM(SOH.TotalDue) AS TotalOrderDue
+FROM Sales.Customer SC
+         LEFT JOIN Sales.SalesOrderHeader SOH ON SC.CustomerID = SOH.CustomerID
+GROUP BY SC.CustomerID, SC.PersonID
+ORDER BY SUM(SOH.TotalDue) DESC
 
 -- 9. Napisz zapytanie, aby znaleźć 5 najlepszych sprzedawców z najwyższą średnią kwotą sprzedaży na zamówienie, i uwzględnij ich Imię, Nazwisko i średnią kwotę sprzedaży na zamówienie. Uporządkuj wyniki według średniej wielkości sprzedaży na zamówienie w kolejności malejącej [Sales.SalesPerson] [Sales.SalesOrderHeader] [Sales.SalesOrderDetail]
 
